ed1893e477d08ce56bfcc19261857f549cc8eff9
Luis Ballesteros <luballe@gmail.com>
2022-06-10 07:52:21 -0500

    commit

--- 2_crear_pickle_sentidos_GUI.sh
+++ 2_crear_pickle_sentidos_GUI.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # Usage: 
-# ./2_crear_pickle_sentidos_GUI.sh /path/to_files EW,WE,NS,SN
-# ./2_crear_pickle_sentidos_GUI.sh /monitoreo/videos_Ago09_Ago13/Ago09/09-08-2021/CL13xKR128/0600-0700/ 128_WE,128_EW,128_ciclo_WE,12_ciclo_EW
+# ./2_crear_pickle_sentidos_GUI.sh /path/to_files/results/ EW,WE,NS,SN
+# ./2_crear_pickle_sentidos_GUI.sh /monitoreo/videos_Ago09_Ago13/Ago09/09-08-2021/CL13xKR128/0600-0700/results/ 128_WE,128_EW,128_ciclo_WE,12_ciclo_EW
+# ./2_crear_pickle_sentidos_GUI.sh /monitoreo/videos/Video_John_Jairo_3/01_martes/01_KR5_CL56_SUR/results/ NS
 pwd_cmd='pwd'
 current_path=$(eval "$pwd_cmd")'/'
 #folder="/monitoreo/Aforos_CGT/2020_06_12/1_AK 7 X AC 72/0600_0800"
